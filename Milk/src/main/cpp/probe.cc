2a5132b9c56d2db9d1b91e545c1f4a9bfd746049
nonwithered <1023001754@qq.com>
2021-06-01 14:37:02 +0800

    feature: milk preview demo

--- Milk/src/main/cpp/probe.cc
+++ Milk/src/main/cpp/probe.cc
@@ -8,29 +8,82 @@ constexpr char kUsage[] =
 "Usage: milk probe [OPTIONS] [FILES]\n"
 "  -h, --help\n"
 "    print this help message\n"
-"  -L {d, i, w, e}\n"
-"  -L {debug, info, warn, error}\n"
-"  --log {d, i, w, e}\n"
-"  --log {debug, info, warn, error}\n"
+"  --log {d, i, w, e, debug, info, warn, error}\n"
 "    init log level, or no log\n"
 "  -x\n"
 "    show all numbers in hex\n"
-"  -c {h, e, d}\n"
-"  --content {header, event, detail}\n"
-"    only headers, or show events with delta-time, or all args\n"
+"  -L {h, e, v, header, events, verbose}\n"
+"  --level {h, e, v, header, events, verbose}\n"
+"    detail level\n"
 "    only headers by default\n"
 ;
 
 class Probe final {
  public:
-  bool ShowHelp(std::list<std::string_view>::iterator &itr, std::list<std::string_view> &args) {
+  Probe()
+    : callbacks_(),
+      help_(false),
+      hex_(false) {
+    callbacks_["-h"] = [this](auto &itr, auto &args) -> bool {
+      ShowHelp();
+      return true;
+    };
+    callbacks_["--help"] = [this](auto &itr, auto &args) -> bool {
+      ShowHelp();
+      return true;
+    };
+    callbacks_["--log"] = [this](auto &itr, auto &args) -> bool {
+      return InitLog(itr, args);
+    };
+    callbacks_["-x"] = [this](auto &itr, auto &args) -> bool {
+      EnableHex();
+      return true;
+    };
+    callbacks_["-L"] = [this](auto &itr, auto &args) -> bool {
+      return DetailLevel(itr, args);
+    };
+    callbacks_["--level"] = [this](auto &itr, auto &args) -> bool {
+      return DetailLevel(itr, args);
+    };
+  }
+  void Launch(std::list<std::string_view> &args) {
+    for (auto itr = args.begin(); itr != args.end(); ) {
+      auto it = callbacks_.find(*itr);
+      if (it != callbacks_.end()) {
+        itr = args.erase(itr);
+        if (!it->second(itr, args)) {
+          ShowHelp();
+          return;
+        }
+      } else {
+        ++itr;
+      }
+    }
+    if (args.size() == 0) {
+      ShowHelp();
+      return;
+    }
+    for (auto it : args) {
+      Preview(it);
+    }
+  }
+ private:
+  std::map<std::string_view, std::function<bool(std::list<std::string_view>::iterator &, std::list<std::string_view> &)>> callbacks_;
+  bool help_;
+  bool hex_;
+  uint8_t detail_ = 0;
+  void ShowHelp() {
+    if (help_) {
+      return;
+    } else {
+      help_ = true;
+    }
     std::cerr << kUsage << std::endl;
-    return true;
   }
   bool InitLog(std::list<std::string_view>::iterator &itr, std::list<std::string_view> &args) {
     MilkPowder_Log_Level_t level = MilkPowder_Log_Level_t::ASSERT;
     if (itr == args.end()) {
-      std::cerr << "milk probe -L: need log level" << std::endl;
+      std::cerr << "milk probe --log: need log level" << std::endl;
       return false;
     } else if (*itr == "d" || *itr == "debug") {
       level = MilkPowder_Log_Level_t::DEBUG;
@@ -42,37 +95,36 @@ class Probe final {
       level = MilkPowder_Log_Level_t::ERROR;
     }
     if (level == MilkPowder_Log_Level_t::ASSERT) {
-      std::cerr << "milk probe -L: invalid level: " << *itr << std::endl;
+      std::cerr << "milk probe --log: invalid log level: " << *itr << std::endl;
       return false;
     }
     LogInitLevel(level);
     itr = args.erase(itr);
     return true;
   }
-  Probe() {
-    callbacks_["-h"] = [this](auto &itr, auto &args) -> bool { return ShowHelp(itr, args); };
-    callbacks_["--help"] = [this](auto &itr, auto &args) -> bool { return ShowHelp(itr, args); };
-    callbacks_["-L"] = [this](auto &itr, auto &args) -> bool { return InitLog(itr, args); };
-    callbacks_["--log"] = [this](auto &itr, auto &args) -> bool { return InitLog(itr, args); };
+  void EnableHex() {
+    hex_ = true;
   }
-  void Launch(std::list<std::string_view> &args) {
-    for (auto itr = args.begin(); itr != args.end(); ) {
-      auto it = callbacks_.find(*itr);
-      if (it != callbacks_.end()) {
-        itr = args.erase(itr);
-        if (!it->second(itr, args)) {
-          return;
-        }
-      } else {
-        ++itr;
-      }
+  bool DetailLevel(std::list<std::string_view>::iterator &itr, std::list<std::string_view> &args) {
+    if (itr == args.end()) {
+      std::cerr << "milk probe --level: need detail level" << std::endl;
+      return false;
+    } else if (*itr == "h" || *itr == "header") {
+      detail_ = 0;
+    } else if (*itr == "e" || *itr == "events") {
+      detail_ = 1;
+    } else if (*itr == "v" || *itr == "verbose") {
+      detail_ = 2;
+    } else {
+      std::cerr << "milk probe --level: invalid detail level: " << *itr << std::endl;
+      return false;
     }
-    for (auto it : args) {
-      std::cout << it << std::endl;
-    }
+    itr = args.erase(itr);
+    return true;
   }
- private:
-  std::map<std::string_view, std::function<bool(std::list<std::string_view>::iterator &, std::list<std::string_view> &)>> callbacks_;
+  void Preview(std::string_view file) {
+      std::cout << file << std::endl;
+  }
 };
 
 } // namespace
