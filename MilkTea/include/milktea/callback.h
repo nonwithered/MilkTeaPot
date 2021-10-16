5799226e6777d6378c1b844551de77a49c2a56b6
nonwithered <1023001754@qq.com>
2021-10-16 14:25:37 +0800

    refactor: rename MilkTea::CallbackToken

--- MilkTea/include/milktea/callback.h
+++ MilkTea/include/milktea/callback.h
@@ -7,19 +7,19 @@
 namespace MilkTea {
 
 template<typename Res, typename... Args>
-struct CallbackOf {
-  CallbackOf(const std::function<Res(Args...)> *callback = nullptr) {}
-  static Res MilkTea_CALL Token(void *obj, Args... args) {
+struct CallbackToken {
+  CallbackToken(const std::function<Res(Args...)> *callback = nullptr) {}
+  static Res MilkTea_CALL Invoke(void *obj, Args... args) {
     std::function<Res(Args...)> &callback_ = *reinterpret_cast<std::function<Res(Args...)> *>(obj);
     return callback_(args...);
   }
 };
 
 template<typename Res, typename... Args>
-struct CallbackOf<Res(Args...)> : public CallbackOf<Res, Args...> {};
+struct CallbackToken<Res(Args...)> : public CallbackToken<Res, Args...> {};
 
 template<typename Res, typename... Args>
-struct CallbackOf<std::function<Res(Args...)>> : public CallbackOf<Res(Args...)> {};
+struct CallbackToken<std::function<Res(Args...)>> : public CallbackToken<Res(Args...)> {};
 
 } // namespace MilkTea
 #endif // ifdef __cplusplus
